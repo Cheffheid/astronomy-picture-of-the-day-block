8e8400c9995d2842cbaa2c174914817d5acd2a34
Jeffrey de Wit <jeffrey@webdevstudios.com>
2020-03-27 13:15:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# NASA Astronomy Picture of the Day
+
+A block for WordPress that will let you display NASA's Astronomy Picture of the Day on your website.
+
+## Development
+This plugin was created with the WebDevStudios Block Starter and includes support for Sass, PostCSS, WebDevStudios Coding Standards, and build tools like [Webpack](https://webpack.js.org), [Babel](https://babeljs.io), and [ESLint](https://eslint.org).
+
+
+Watch for changes
+
+```bash
+npm start
+```
+
+Build the production version of the plugin
+
+```bash
+npm run build
+```
+
+### Other handy commands
+
+Lint JS
+
+```bash
+npm run lint:js
+```
+
+Lint CSS
+
+```bash
+npm run lint:css
+```
+
+Lint PHP
+
+```bash
+npm run lint:php
+```
+
+For more info on the WordPress Block API, check out the [Gutenberg Handbook](https://developer.wordpress.org/block-editor/).
+
+# Changelog
+
+## 0.0.1
+- Initial block setup.
---
+++ astronomy-photo-of-the-day.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Plugin Name:     Astronomy Picture Of The Day
+ * Description:     A block for WordPress that will let you display NASA's Astronomy Picture of the Day on your website.
+ * Version:         0.1.0
+ * Author:          Jeffrey de Wit
+ * License:         GPL-2.0-or-later
+ * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:     astronomy-picture-of-the-day
+ *
+ * @package         Cheffism\AstronomyPictureOfTheDay
+ */
+
+namespace Cheffism\AstronomyPictureOfTheDay;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Register the block with WordPress.
+ *
+ * @author Cheffism
+ * @since 0.0.1
+ */
+function register_block() {
+
+	// Define our assets.
+	$editor_script   = 'build/index.js';
+	$editor_style    = 'build/editor.css';
+	$frontend_style  = 'build/style.css';
+	$frontend_script = 'build/frontend.js';
+
+	// Verify we have an editor script.
+	if ( ! file_exists( plugin_dir_path( __FILE__ ) . $editor_script ) ) {
+		wp_die( esc_html__( 'Whoops! You need to run `npm run build` for the Cheffism Astronomy Picture Of The Day first.', 'astronomy-picture-of-the-day' ) );
+	}
+
+	// Autoload dependencies and version.
+	$asset_file = require plugin_dir_path( __FILE__ ) . 'build/index.asset.php';
+
+	// Register editor script.
+	wp_register_script(
+		'astronomy-picture-of-the-day-editor-script',
+		plugins_url( $editor_script, __FILE__ ),
+		$asset_file['dependencies'],
+		$asset_file['version'],
+		true
+	);
+
+	// Register editor style.
+	if ( file_exists( plugin_dir_path( __FILE__ ) . $editor_style ) ) {
+		wp_register_style(
+			'astronomy-picture-of-the-day-editor-style',
+			plugins_url( $editor_style, __FILE__ ),
+			[ 'wp-edit-blocks' ],
+			filemtime( plugin_dir_path( __FILE__ ) . $editor_style )
+		);
+	}
+
+	// Register frontend style.
+	if ( file_exists( plugin_dir_path( __FILE__ ) . $frontend_style ) ) {
+		wp_register_style(
+			'astronomy-picture-of-the-day-style',
+			plugins_url( $frontend_style, __FILE__ ),
+			[],
+			filemtime( plugin_dir_path( __FILE__ ) . $frontend_style )
+		);
+	}
+
+	// Register block with WordPress.
+	register_block_type( 'cheffism/astronomy-picture-of-the-day', array(
+		'editor_script' => 'astronomy-picture-of-the-day-editor-script',
+		'editor_style'  => 'astronomy-picture-of-the-day-editor-style',
+		'style'         => 'astronomy-picture-of-the-day-style',
+	) );
+
+	// Register frontend script.
+	if ( file_exists( plugin_dir_path( __FILE__ ) . $frontend_script ) ) {
+		wp_enqueue_script(
+			'astronomy-picture-of-the-day-frontend-script',
+			plugins_url( $frontend_script, __FILE__ ),
+			$asset_file['dependencies'],
+			$asset_file['version'],
+			true
+		);
+	}
+}
+add_action( 'init', __NAMESPACE__ . '\register_block' );
