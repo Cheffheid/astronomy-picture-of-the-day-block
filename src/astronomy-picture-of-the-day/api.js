4c8a2d879efcd66536472618c88a02f8c40f00f4
Jeffrey de Wit <jeffrey@webdevstudios.com>
2020-03-27 15:16:51 -0400

    Add api file for API requests

---
+++ src/astronomy-picture-of-the-day/api.js
@@ -0,0 +1,13 @@
+/**
+ * Retrieve the Astronomy Picture of the Day in HD.
+ *
+ * @param {string} apiKey NASA API Key. Register at https://api.nasa.gov/.
+ */
+const getPictureOfTheDay = async ( apiKey ) => {
+	const response = await fetch( `https://api.nasa.gov/planetary/apod?api_key=${apiKey}` );
+	const imageData = await response.json();
+
+	return imageData.url;
+}
+
+export default getPictureOfTheDay;
