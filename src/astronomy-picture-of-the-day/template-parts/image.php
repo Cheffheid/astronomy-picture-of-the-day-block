8971491f0a5e638207d651807464851e3045b455
Jeffrey de Wit <jeffrey@webdevstudios.com>
2020-04-24 14:51:33 -0400

    Add image template part

---
+++ src/astronomy-picture-of-the-day/template-parts/image.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Image template part used in render_astronomy_picture_of_the_day().
+ *
+ * @package Cheffism\AstronomyPictureOfTheDay
+ */
+
+?>
+
+<p class="astronomy-picture-of-the-day">
+	<img src="<?php echo esc_url( $picture_url ); ?>" alt="" />
+</p>
