c459732a8dd38d30023fad7bf1a2425638472b4c
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-05-31 11:03:08 -0400

    Add a section wrapper around the block's output

--- src/render.php
+++ src/render.php
@@ -17,6 +17,11 @@ if ( is_wp_error( $picture_data ) ) {
 $picture_url        = $picture_data->url;
 $media_include_file = __DIR__ . '/template-parts/' . $picture_data->media_type . '.php';
 
-if ( file_exists( $media_include_file ) ) {
-	require $media_include_file;
-}
+if ( file_exists( $media_include_file ) ) :
+	?>
+	<section <?php echo get_block_wrapper_attributes(); // phpcs:ignore  ?>>
+		<?php require $media_include_file; ?>
+	</section>
+
+	<?php
+endif;
